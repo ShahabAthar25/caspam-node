d624bc2dc8a53a4a7ebd130fa314bbeee7653372
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-13 16:34:40 +0500

    fix ordering of imports

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
-import { Router } from "express"
+import { Router } from "express";
+import { register, login, refresh, logout } from "../controllers/auth.js";
 
-const router = Router()
-
-import { register, login, refresh, logout } from "../controllers/auth.js"
+const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
 router.delete("/logout", logout);
 
-export default router
+export default router;
