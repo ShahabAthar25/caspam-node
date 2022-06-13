25c8c7c75905a8c2e13fe5b1bdc955809c587af5
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-13 16:42:54 +0500

    add validation

---
+++ utils/validation.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+export const registerValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().required().min(3).max(60),
+    email: Joi.string().required().min(3).max(60).email(),
+    password: Joi.string().required().min(8),
+  });
+
+  return schema.validate(data);
+};
+
+export const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  });
+
+  return schema.validate(data);
+};
