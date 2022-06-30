18d59d8d3e1080b8bbb991e6db0ebb6b5f1e6671
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-30 08:36:24 +0500

    Add year view

--- routes/blog.js
+++ routes/blog.js
@@ -9,6 +9,7 @@ import {
   categoryView,
   dayView,
   monthView,
+  yearView,
 } from "../controllers/blog.js";
 
 import protectedRoute from "../middleware/protected.js";
@@ -20,7 +21,7 @@ router.get("/:id", blogDetailView);
 router.get("/category/:category", categoryView);
 router.get("/archives/today", dayView);
 router.get("/archives/month", monthView);
-router.get("/archives/year", likeBlog);
+router.get("/archives/year", yearView);
 router.post("/", protectedRoute, createBlog);
 router.put("/:id", protectedRoute, updateBlog);
 router.delete("/:id", protectedRoute, deleteBlog);
